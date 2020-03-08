5927420aec10fa1daeb1bc412ee56fbf0fe2c95c
Bruno Camargos <brunocamargos2009@hotmail.com>
2020-03-08 20:47:58 -0300

    first commit course

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,57 @@
+package br.com.casadocodigo.loja.controllers;
+
+import br.com.casadocodigo.loja.validation.ProdutoValdation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("produtos")
+public class ProdutosController {
+	
+	@Autowired
+	private ProdutoDAO dao;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.addValidators(new ProdutoValdation());
+	}
+
+	@RequestMapping("/form")
+	public ModelAndView form() {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		return modelAndView;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView gravar(@Valid Produto produto,BindingResult result, RedirectAttributes redirectAttributes){
+		if (result.hasErrors()){
+			return form();
+		}
+		dao.gravar(produto);
+		redirectAttributes.addFlashAttribute("sucesso", "Produto Cadastrado Com Sucesso!");
+		return new ModelAndView("redirect:produtos");
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView listar(){
+		List<Produto>produtos = dao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
+	}
+}
---
+++ src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -0,0 +1,27 @@
+package br.com.casadocodigo.loja.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class ProdutoDAO {
+
+	@PersistenceContext
+	EntityManager manager;
+
+	public void gravar(Produto produto) {
+		manager.persist(produto);
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("select p from Produto p",Produto.class).getResultList();
+	}
+}
